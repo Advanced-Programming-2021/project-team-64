b4b8725e475d22c47194af336f659b90dae2b941
Mahdi_Hajibeygi <mahdi.hb1380@gmail.com>
2021-05-07 11:12:34 +0430

    add comment of database actions

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-import com.google.gson.*;
 public class Main {
     public static void main(String[] args) {
+        // read data from database
         System.out.println("hello");
 
+        // write data to database
     }
 }
