e3cede2b079f7a1821338c5eaadfd3cd0dd6380e
Mahdi_Hajibeygi <mahdi.hb1380@gmail.com>
2021-05-07 11:15:25 +0430

    don't extend model instead we will use functions in it for reading and writing data

--- Models/Model.java
+++ Models/Model.java
@@ -2,4 +2,5 @@ package Models;
 public class Model {
     // here we will have all common functions for models
     // like importing and exporting datas and etc.
+    // static functions for database actions
 }
