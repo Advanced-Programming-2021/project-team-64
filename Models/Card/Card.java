e8fbdaa58a6a6eebca28f93442f1e71d4599aba1
Mahdi_Hajibeygi <mahdi.hb1380@gmail.com>
2021-05-08 10:35:10 +0430

    add checkName and make Card comparable

--- Models/Card/Card.java
+++ Models/Card/Card.java
@@ -4,7 +4,7 @@ import Models.Model;
 
 import java.util.ArrayList;
 
-public interface Card {
+public interface Card extends Comparable<Card> {
     String name = "";
     String attribute = "";
     String type = "";
@@ -13,5 +13,13 @@ public interface Card {
     Integer cardNumber = 0;
     Integer attack = 0;
     Integer defence = 0;
-    public boolean checkName (String cardName);
+
+    default public boolean checkName (String cardName) {
+        return this.name.equals(cardName);
+    }
+
+    @Override
+    default int compareTo(Card o) {
+        return this.toString().compareTo(o.toString());
+    }
 }
